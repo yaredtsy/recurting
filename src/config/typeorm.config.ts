5f0875ba0bc4cb5b8941e915ed9c24805368b740
yaredtsy <34584266+yaredtsy@users.noreply.github.com>
2022-06-02 15:20:17 +0300

    deploymrnt added

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: 'containers-us-west-55.railway.app',
+  port: 7501,
   username: 'postgres',
-  password: 'root',
-  database: 'rhuk',
+  password: '6Bx7xKm5On8ANIpv8I4u',
+  database: 'railway',
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: true,
   autoLoadEntities: true,
